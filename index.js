4590942c7fc508180c9b8f39b385ecfbd25a283b
danedehaan <danedehaan@qubitsec.com>
2020-05-25 17:28:36 +0900

    로그인시 자동 입력 방지가 뜬다...너무 안일했네

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const puppeteer = require("puppeteer");
+const auth = require("./auth.json");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://www.naver.com/");
-  console.log("page", page);
-  page.getEle;
+  await page.goto("https://nid.naver.com/nidlogin.login");
 
-  await page.evaluate(() => {
-    const accountWrap = document.getElementById("account");
-    accountWrap.getElementsByClassName("link_login")[0].click();
-  });
+  await page.evaluate(async (auth) => {
+    const id_input = document.getElementById("id");
+    const pw_input = document.getElementById("pw");
+
+    id_input.value = auth.id;
+    pw_input.value = auth.pw;
+    const btn_login = document.getElementById("log.login");
+    btn_login.click();
+  }, auth);
 
   await page.screenshot({ path: "naver.png" });
   browser.close();
